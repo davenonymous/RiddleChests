c61d01419ad91a2841a6d382acdca3a30462ec1d
thraaawn <david.hilberath@gmx.de>
2020-01-12 16:15:19 +0100

    Create PKCS12 keystore

--- update-signature.sh
+++ update-signature.sh
@@ -3,19 +3,20 @@
 ALIAS=davenonymous/riddlechests
 
 KEYTOOL_BIN=keytool
+KEYTOOL_BIN=/f/dev/java/bin/keytool.exe
 
-rm -f ./keystore.jks
+rm -f ./keystore.pfx
 ${KEYTOOL_BIN} -genkey -alias ${ALIAS} \
-	-keyalg RSA -keysize 2048 -keystore ./keystore.jks \
+	-keyalg RSA -keysize 2048 -keystore ./keystore.pfx -storetype PKCS12 \
 	-storepass ${JARSIGN_KEY_PASS} -keypass ${JARSIGN_KEY_PASS} \
 	-noprompt -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, ST=Unknown, C=Unknown"
 
 echo "Created key store file"
 
-hash=$(${KEYTOOL_BIN} -list -alias davenonymous/riddlechests -keystore ./keystore.jks -storepass ${JARSIGN_KEY_PASS} | grep SHA1 | awk '{print $4}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')
+hash=$(${KEYTOOL_BIN} -list -alias davenonymous/riddlechests -keystore ./keystore.pfx -storepass ${JARSIGN_KEY_PASS} | grep SHA1 | awk '{print $4}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')
 
 echo "" >> gradle.properties
-echo keyStore=./keystore.jks >> gradle.properties
+echo keyStore=./keystore.pfx >> gradle.properties
 echo keyStoreAlias=${ALIAS} >> gradle.properties
 echo signSHA1=${hash} >> gradle.properties
 

afa9afa24f202b1a23133375e6abfb9b92714acc
thraaawn <david.hilberath@gmx.de>
2020-01-12 15:50:07 +0100

    Sign the jar

---
+++ update-signature.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ALIAS=davenonymous/riddlechests
+
+KEYTOOL_BIN=keytool
+
+rm -f ./keystore.jks
+${KEYTOOL_BIN} -genkey -alias ${ALIAS} \
+	-keyalg RSA -keysize 2048 -keystore ./keystore.jks \
+	-storepass ${JARSIGN_KEY_PASS} -keypass ${JARSIGN_KEY_PASS} \
+	-noprompt -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, ST=Unknown, C=Unknown"
+
+hash=$(${KEYTOOL_BIN} -list -alias davenonymous/riddlechests -keystore ./keystore.jks -storepass ${JARSIGN_KEY_PASS} | grep SHA1 | awk '{print $4}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')
+
+echo "" >> gradle.properties
+echo keyStore=./keystore.jks >> gradle.properties
+echo keyStoreAlias=${ALIAS} >> gradle.properties
+echo signSHA1=${hash} >> gradle.properties

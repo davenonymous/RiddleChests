776c924523b9b6402ce29df4360d449eabee7dbb
thraaawn <david.hilberath@gmx.de>
2020-01-12 16:05:21 +0100

    Add debug output

--- update-signature.sh
+++ update-signature.sh
@@ -10,9 +10,13 @@ ${KEYTOOL_BIN} -genkey -alias ${ALIAS} \
 	-storepass ${JARSIGN_KEY_PASS} -keypass ${JARSIGN_KEY_PASS} \
 	-noprompt -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, ST=Unknown, C=Unknown"
 
+echo "Created key store file"
+
 hash=$(${KEYTOOL_BIN} -list -alias davenonymous/riddlechests -keystore ./keystore.jks -storepass ${JARSIGN_KEY_PASS} | grep SHA1 | awk '{print $4}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')
 
 echo "" >> gradle.properties
 echo keyStore=./keystore.jks >> gradle.properties
 echo keyStoreAlias=${ALIAS} >> gradle.properties
 echo signSHA1=${hash} >> gradle.properties
+
+echo "Created signing hash: ${hash}"
